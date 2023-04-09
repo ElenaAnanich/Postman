ff2cebd6452cd0481797beae225212e6c3004ed8
Lena k <ktyecz09@mail.ru>
2023-04-09 09:14:58 +0300

    add initial commit

---
+++ src/test/java/org/example/test.java
@@ -0,0 +1,20 @@
+package org.example;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+class test {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"));
+    }
+}
